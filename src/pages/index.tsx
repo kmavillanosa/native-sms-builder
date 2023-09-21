9fc973bf689daf855521e538ac63c1863c5fb965
Kim Avillanosa <avillanosakzxc@gmail.com>
2023-09-21 16:59:25 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,10 @@ import { useState } from "react";
 
 export default function Home() {
   const [body, setBody] = useState("");
+  const [phone, setPhone] = useState("+639452873791");
 
   const handleSendSMS = () => {
-    const smsLink = `sms:+639452873791&body=${encodeURIComponent(body)}`;
+    const smsLink = `sms:${phone}&body=${encodeURIComponent(body)}`;
 
     // Check if the device is running iOS (iPhone or iPad)
     const isiOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
@@ -18,7 +19,7 @@ export default function Home() {
       window.open(smsLink);
     } else if (isAndroid) {
       // For Android devices, open an intent to send an SMS
-      window.location.href = `sms:+639452873791?body=${encodeURIComponent(body)}`;
+      window.location.href = `sms:${phone}?body=${encodeURIComponent(body)}`;
     } else {
       // For other devices, use the 'sms:' protocol
       window.location.href = smsLink;
@@ -28,7 +29,8 @@ export default function Home() {
   return (
     <Page title="Welcome to NextLaunch">
       <Layout>
-        <input value={body} onChange={(e) => setBody(e.target.value)} />
+        <input placeholder="Phone number here" value={phone} onChange={(e) => setPhone(e.target.value)} />
+        <input placeholder="Body here" value={body} onChange={(e) => setBody(e.target.value)} />
         <button onClick={handleSendSMS}>Send SMS</button>
       </Layout>
     </Page>
