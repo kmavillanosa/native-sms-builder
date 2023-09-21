12bf78a52924715970590b43f98e65beded26718
Kim Avillanosa <avillanosakzxc@gmail.com>
2023-09-21 16:43:02 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
 import { Layout, Page } from "@/shared/components";
 import Link from "next/link";
-
+import { useState } from "react";
 
 export default function Home() {
+  const [body, setBody] = useState("");
+
   return (
     <Page title="Welcome to NextLaunch">
       <Layout>
-        <a href="sms:+639452873791?body=sample">Send SMS</a>
+        <input value={body} onChange={(e) => setBody(e.target.value)} />
+        <a href={`sms:+639452873791?body=${encodeURIComponent(body)}`}>
+          Send SMS
+        </a>
       </Layout>
     </Page>
   );
