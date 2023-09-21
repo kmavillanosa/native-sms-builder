193f4e8dc4e35b0ceb047a69a3ecac7cf52f3262
Kim Avillanosa <avillanosakzxc@gmail.com>
2023-09-21 16:46:12 +0800

    modified for mobile support

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,40 @@
 import { Layout, Page } from "@/shared/components";
-import Link from "next/link";
 import { useState } from "react";
 
 export default function Home() {
   const [body, setBody] = useState("");
 
+  const handleSendSMS = () => {
+    const smsLink = `sms:+639452873791&body=${encodeURIComponent(body)}`;
+
+    // Check if the device is running iOS (iPhone or iPad)
+    const isiOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
+
+    // Check if the device is running Android
+    const isAndroid = /Android/.test(navigator.userAgent);
+
+    if (isiOS) {
+      // For iOS devices, open the link using window.open
+      window.open(smsLink);
+    } else if (isAndroid) {
+      // For Android devices, open an intent to send an SMS
+      window.location.href = `sms:+639452873791?body=${encodeURIComponent(body)}`;
+    } else {
+      // For other devices, use the 'sms:' protocol
+      window.location.href = smsLink;
+    }
+  };
+
   return (
     <Page title="Welcome to NextLaunch">
       <Layout>
         <input value={body} onChange={(e) => setBody(e.target.value)} />
-        <a href={`sms:+639452873791?body=${encodeURIComponent(body)}`}>
-          Send SMS
-        </a>
+        <button onClick={handleSendSMS}>Send SMS</button>
       </Layout>
     </Page>
   );
 }
+
+
+
+
